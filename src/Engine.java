ab66e5b36f61e0f5145176d52fdc4cea39cbcad6
cjy5363 <cjy5363@psu.edu>
2026-01-22 11:44:39 -0500

    added mycapture and engine

---
+++ src/Engine.java
@@ -0,0 +1,27 @@
+public class Engine {
+    private String cc;
+    private String hp;
+
+    public Engine(String cc){
+        this.cc = cc;
+    }
+    public Engine(String cc, String hp){
+        this.cc = cc;
+        this.hp = hp;
+    }
+
+    public void setCC(String cc){
+        this.cc = cc;
+    }
+    public String getCC(){
+        return this.cc;
+    }
+    public void setHP(String hp){
+        this.hp = hp;
+    }
+    public String getHP(){
+        return this.hp;
+    }
+
+
+}
